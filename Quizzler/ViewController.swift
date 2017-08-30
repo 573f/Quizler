b9e631a3baf378d5471b38d93999a1b53ca4c347
Stephen Skubik-Peplaski <codesmith@me.com>
2017-08-30 17:42:28 -0400

    Completed Project

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     let allQuestions = QuestionBank()
     var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
+    var score: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,40 +22,62 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = allQuestions.list[0].questionText
+        updateUI()
     }
-
-
+    
     @IBAction func answerPressed(_ sender: AnyObject) {
         pickedAnswer = sender.tag == 1
         checkAnswer()
+        nextQuestion()
     }
-    
     
     func updateUI() {
-      
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1)/13"
+        progressBar.frame.size.width = (view.frame.size.width / CGFloat(allQuestions.list.endIndex)) * CGFloat(questionNumber + 1)
     }
     
-
     func nextQuestion() {
+        if questionNumber < allQuestions.list.endIndex - 1 {
+            questionNumber += 1
+            updateUI()
+        } else {
+            let alert = UIAlertController(
+                title: "Awesome",
+                message: "You've finished all the questions, do you want to start over?",
+                preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(
+                title: "Restart",
+                style: .default,
+                handler: {
+                    (UIAlertAction) in
+                    self.startOver()
+            })
+            
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
         
     }
     
-    
     func checkAnswer() {
-        let correctAnswer = allQuestions.list[0].answer
+        let correctAnswer = allQuestions.list[questionNumber].answer
         if correctAnswer == pickedAnswer {
-            print("You got it!")
+            ProgressHUD.showSuccess("Correct")
+            score += 1
         } else {
-            print("Nope!")
+            ProgressHUD.showError("Wrong!")
         }
     }
     
-    
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        updateUI()
     }
     
-
+    
     
 }
