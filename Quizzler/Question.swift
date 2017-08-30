e5c9b83169501ab077a30ba42e0b85dbf63a2455
Stephen Skubik-Peplaski <codesmith@me.com>
2017-08-30 16:13:34 -0400

    Added Question and QuestionBank models

--- Quizzler/Question.swift
+++ Quizzler/Question.swift
@@ -1,8 +1,18 @@
 //
 //  Question.swift
-//  
+//  Quizzler
 //
 //  Created by Stephen Skubik-Peplaski on 8/30/17.
 //
 
 import Foundation
+
+class Question {
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
